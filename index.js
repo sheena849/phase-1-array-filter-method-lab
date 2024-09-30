336dc9f434570ec209fc59d82f0479b6268e1b41
sheena849 <sheenamugo04@gmail.com>
2024-09-30 11:27:50 +0300

    Array Filter

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(source,sought){
+    return source.filter(
+        (possibleMatch)=>possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+}
+function fuzzyMatch(source, testString) {
+    return source.filter(
+        (possibleMatch) => 
+            possibleMatch.toLowerCase().startsWith(testString.toLowerCase())
+    );
+}
+
+function matchName(source,soughtName){
+    return source.filter(
+        (driver)=>driver.name ===soughtName)
+}
